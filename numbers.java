d1ee915df4ce247969c507942420030ebca6add3
govarshini <govarshiniarumugam@gmail.com>
2017-06-09 18:28:25 +0530

    Create numbers.java

---
+++ numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class numbers
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%2==0)
+		System.out.println("the number is even");
+		else
+			System.out.println("the number is odd");
+		
+	}	
+	}
